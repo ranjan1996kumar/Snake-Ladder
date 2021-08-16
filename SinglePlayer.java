3ffb4aafda12710aa4b20124a43989f9a7549a32
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-08-16 15:30:16 +0530

    Played game with single player

---
+++ SinglePlayer.java
@@ -0,0 +1,12 @@
+public class SinglePlayer {
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake And ladder Program");
+        int START = 0;
+        int FINISH = 100;
+        int playerPos;
+
+        System.out.println("Game Start");
+        playerPos = START;
+        System.out.println("player at Position : " + playerPos);
+    }
+}
