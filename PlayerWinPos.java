9e3845f1e773874f9a592a73d4c156fbd1dae53b
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-08-16 15:39:54 +0530

    Repeat till the player reaches the winning po 100

---
+++ PlayerWinPos.java
@@ -0,0 +1,37 @@
+public class PlayerWinPos {
+    public static void main(String[] args) {
+        int position=0;
+        while (position != 100) {
+            int diceRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Dice Rolled:"+diceRoll);
+
+            int checkOption=(int)Math.floor(Math.random()*10)%3;
+            if (checkOption == 0) {
+                System.out.println("No Play");
+            }
+            else if (checkOption == 1) {
+                System.out.println("Ladder Comes");
+                if(position+diceRoll<=100)
+                {
+                    position+=diceRoll;
+                }
+                else if(position+diceRoll>100)
+                {
+                    continue;
+                }
+            }
+            else {
+                System.out.println("Snake Comes");
+                if((position-diceRoll)<0)
+                {
+                    position=0;
+                }
+                else
+                {
+                    position-=diceRoll;
+                }
+            }
+        }
+
+    }
+}
