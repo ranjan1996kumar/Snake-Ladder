d10de7986dbb935f14b5343be535aa4518ef99f4
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-08-16 15:33:02 +0530

    Player rolls dice to get a no

---
+++ RollsDice.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class RollsDice{
+    public static void main(String[] args) {
+            int position=0;
+            int diceRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Dice Rolled:"+diceRoll);
+        }
+
+    }
